5641e7402ed746da98e9af3684bb46248983fbbd
smriti <iamsmritid@gmail.com>
2018-02-13 02:26:56 +0530

    home page completed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CoveragePage } from './../coverage/coverage';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  movetocoverage(){
+  this.navCtrl.push(CoveragePage);
   }
 
 }

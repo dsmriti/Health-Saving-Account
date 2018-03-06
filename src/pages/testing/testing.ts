e769984343013c75c7b9b62351fc119cb23d3f96
smriti <iamsmritid@gmail.com>
2018-03-07 00:38:01 +0530

    file read on page n console

--- src/pages/testing/testing.ts
+++ src/pages/testing/testing.ts
@@ -2,49 +2,63 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import * as XLSX from 'xlsx';
 import { File } from '@ionic-native/file';
-/**
- * Generated class for the TestingPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-type AOA = any[][];
 @Component({
   selector: 'page-testing',
   templateUrl: 'testing.html',
 })
 export class TestingPage {
-  data: any[][] = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams,public file: File) {
+  page_data: any[][] = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public file: File) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TestingPage');
+    this.XLStoJSON();
   }
-  read(bstr: string) {
-    /* read workbook */
-    const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
-
-    /* grab first sheet */
-    const wsname: string = wb.SheetNames[0];
-    const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-
-    /* save data */
-    this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1}));
-  };
-  /* File Input element for browser */
-  onFileChange(evt: any) {
-    /* wire up file reader */
-    const target: DataTransfer = <DataTransfer>(evt.target);
-    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
-    const reader: FileReader = new FileReader();
-    reader.onload = (e: any) => {
-      const bstr: string = e.target.result;
-      this.read(bstr);
-    };
-    reader.readAsBinaryString(target.files[0]);
-  };
 
 
+  public XLStoJSON() {
+    return new Promise((resolve, reject) => {
+      var url = 'http://localhost:8100/src/hsa.xlsx';
+      var oReq = new XMLHttpRequest();
+      var workbook: any;
+      oReq.open("GET", url, true);
+      oReq.responseType = "arraybuffer";
+      oReq.onload = (e) => {
+        if (oReq.status >= 200 && oReq.status < 300) {
+          var arraybuffer = oReq.response;
+          var data = new Uint8Array(arraybuffer);
+          var arr = new Array();
+          for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+          var bstr = arr.join("");
+          workbook = XLSX.read(bstr, {type: "binary"});
+          var worksheetname = workbook.SheetNames[0];
+          var worksheet = workbook.Sheets[worksheetname];
+          var json = XLSX.utils.sheet_to_json(worksheet, {raw: true});
+          this.arrangeData(json);
+          resolve('Finished generating JSON');
+        } else {
+          reject(console.log('XMLHttpRequest failed; error code:' + oReq.statusText));
+        }
+      }
+      oReq.send();
+    });
+  }
+
+  public arrangeData(jsonData) {
+    console.log(jsonData);
+    var temparray = [];
+
+    for (var i = 0; i != jsonData.length; ++i) {
+      var currentarray = [];
+      currentarray.push(jsonData[i]["Parameter"]);
+      currentarray.push(jsonData[i]["Content"]);
+      currentarray.push(jsonData[i]["Valid Values / Notes"]);
+      temparray.push(currentarray)
+    }
+
+    this.page_data = temparray;
+  }
 }
